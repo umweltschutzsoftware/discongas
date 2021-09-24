7b5f71ed4dd50fc75cf4641d06f28f126b7f852a
umweltschutzsoftware <jannik.huels@richtershuels.de>
2021-09-24 14:00:57 +0200

    moved roofpitchcorrection to util roof

--- discongas/undisturbedremoval.py
+++ discongas/undisturbedremoval.py
@@ -2,44 +2,6 @@ import math
 from discongas.util.inputoutput import *
 from discongas.util.roof import *
 
-def roofpitchcorrection(alpha):
-  """
-  Calculate the roof pitch correction gamma and factor f as a function of the roof pitch alpha.
-
-  :param alpha: roof pitch:
-
-  :return: a tuple (gamma, f)
-  """
-  if alpha < 20:
-    return (0, 0)
-  if alpha >= 20 and alpha <= 30:
-    gamma = 0
-
-    # multiplier to interpolate
-    m = (alpha-20)/10
-
-    f = round(0.85 - 0.15 * m, 2)
-
-  elif alpha > 30 and alpha <= 45:
-    # multiplier to interpolate
-    m = (alpha-30)/15
-
-    gamma = round(10 * m, 2)
-    f = round(0.7 - 0.2 * m, 2)
-
-  elif alpha > 45 and alpha < 60:
-    # multiplier to interpolate
-    m = (alpha-45)/15
-
-    gamma = round(10 + 10 * m, 2)
-    f = round(0.5 - 0.05 * m, 2)
-
-  else:
-    gamma = 20
-    f = 0.45
-    
-  return (gamma, f)
-
 def symmetricpitchedroofs(a, H_Dach, b, nominalheatoutput=400, ratedthermalinput=1):
   """
   Outlet height for symmetric pitched roofs.
