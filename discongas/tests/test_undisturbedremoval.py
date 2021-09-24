33c1d5a76070df2f61f8c3345d8daf4237670a8e
umweltschutzsoftware <jannik.huels@richtershuels.de>
2021-09-24 15:30:46 +0200

    outlet height for single pitch roofs

--- discongas/tests/test_undisturbedremoval.py
+++ discongas/tests/test_undisturbedremoval.py
@@ -3,19 +3,29 @@ from discongas.undisturbedremoval import *
 def test_symmetricpitchedroofs():
   # test according to annax A1.2
   # X1
-  assert(symmetricpitchedroofs(0, 3.7, 11.3) == 0.4)
+  assert(symmetricpitchedroofs(0, 3.7, 5.65) == 0.4)
   # X2 
-  assert(symmetricpitchedroofs(5.7, 3.7, 11.3) == 2.8)
+  assert(symmetricpitchedroofs(5.7, 3.7, 5.65) == 2.8)
   # X3 
-  assert(symmetricpitchedroofs(0, 3.7, 11.3) == 0.4)
+  assert(symmetricpitchedroofs(0, 3.7, 5.65) == 0.4)
 
   # test with a roof angle == 20
-  assert(symmetricpitchedroofs(0, 3.7, 11.3) == 0.4)
+  assert(symmetricpitchedroofs(0, 3.7, 5.65) == 0.4)
 
   # test with a roof angle < 20
-  assert(symmetricpitchedroofs(0, 2.45, 15.6) == 0.8)
+  assert(symmetricpitchedroofs(0, 2.45, 7.8) == 0.8)
 
 def test_flatroofs():
   # test according to annax A2.1
   # only the rooftop building is considered
-  assert(flatroofs(4, 4) == 1.1)
+  assert(flatroofs(4, 2) == 1.1)
+
+def test_singlepitchroof():
+  # test according to annax A2.3
+  assert(singlepitchroof(0, 3.7, 8)==0.4)
+  assert(singlepitchroof(4, 3.7, 8)==1.9)
+  assert(singlepitchroof(8, 3.7, 8)==1.9)
+
+  assert(singlepitchroof(0, 2.5, 8) == 0.6)
+  assert(singlepitchroof(4, 2.5, 8) == 1.9)
+  assert(singlepitchroof(8, 2.5, 8) == 1.9)
--- discongas/undisturbedremoval.py
+++ discongas/undisturbedremoval.py
@@ -2,13 +2,13 @@ import math
 from discongas.util.inputoutput import *
 from discongas.util.roof import *
 
-def symmetricpitchedroofs(a, H_Dach, b, nominalheatoutput=400, ratedthermalinput=1, flatroof=False):
+def symmetricpitchedroofs(a, H_Dach, dridge, nominalheatoutput=400, ratedthermalinput=1, flatroof=False):
   """
   Outlet height for symmetric pitched roofs.
 
   :param a: horizontal distance beween the centre of the outlet cross-section and the ridge (in m):
   :param H_Dach: the buildings actual roof height (in m):
-  :param b: Width of the buildings gable end or narrow side of the building for flat roofs (in m):
+  :param dridge: Horizontal distance from the gable to the ridge (in m):
   :param nominalheatoutput: Nominal heat output (in kW):
   :param ratedthermalinput: Rated thermal input (in MW):
   :param flatroof: Indicates wether a flat roof should be considered (Boolean):
@@ -16,7 +16,7 @@ def symmetricpitchedroofs(a, H_Dach, b, nominalheatoutput=400, ratedthermalinput
   :return: height H_A1
   """
   if (not flatroof):
-    alpha = roofangle(H_Dach, b/2)
+    alpha = roofangle(H_Dach, dridge)
   else:
     alpha = 0
 
@@ -26,16 +26,16 @@ def symmetricpitchedroofs(a, H_Dach, b, nominalheatoutput=400, ratedthermalinput
     H_1 = a * math.tan((alpha-gamma)*math.pi/180)
     H_2 = f * H_Dach
   else:
-    if (b == 0):
+    if (dridge == 0):
       raise TypeError("The width b needs to be defined if roof angle alpha is less than 20 degree.")
     f = alpha/20*0.85
-    H_1 = (a+b/2)*math.tan(20*math.pi/180)-H_Dach
-    H_2 = (1+f)*b/2*math.tan(20*math.pi/180)-H_Dach
+    H_1 = (a+dridge)*math.tan(20*math.pi/180)-H_Dach
+    H_2 = (1+f)*dridge*math.tan(20*math.pi/180)-H_Dach
 
   H_S1 = min(H_1, H_2)
   H_Ü = additiveterm(nominalheatoutput, ratedthermalinput)
 
-  return round(H_S1 + H_Ü,1)
+  return round(H_S1 + H_Ü, 1)
 
 def flatroofs(H_First, b, nominalheatoutput=400, ratedthermalinput=1):
   """
@@ -52,3 +52,28 @@ def flatroofs(H_First, b, nominalheatoutput=400, ratedthermalinput=1):
   H_Ü = additiveterm(nominalheatoutput, ratedthermalinput)
   H_A1F = round(1.3*((H_First**2)**(1./3.))+H_Ü,1)
   return min(H_A1, H_A1F)
+
+def singlepitchroof(a, H_Dach, b, nominalheatoutput=400, ratedthermalinput=1):
+  """
+  Outlet height for single pitch roofs.
+
+  :param H_Dach: the buildings actual roof height (in m):
+  :param b: Width of the buildings gable (in m):
+  :param nominalheatoutput: Nominal heat output (in kW):
+  :param ratedthermalinput: Rated thermal input (in MW):
+
+  :return: height H_A1
+  """
+  alpha = roofangle(H_Dach, b)
+  if (alpha >= 20):
+    c = 0
+  else: 
+    c=(b/2-a)*(1-alpha/20)
+
+  H_1 = (a+c)*math.tan(20*math.pi/180)
+  H_2 = b/2*math.tan(20*math.pi/180)
+
+  H_S1 = min(H_1, H_2)
+  H_Ü = additiveterm(nominalheatoutput, ratedthermalinput)
+
+  return round(H_S1 + H_Ü, 1)
