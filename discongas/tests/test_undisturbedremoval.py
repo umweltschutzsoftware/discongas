c6d8d99e8a11a208a6cc0b3a3fa9d34afb9df101
umweltschutzsoftware <jannik.huels@richtershuels.de>
2021-09-24 11:17:24 +0200

    roof pitch correction according to table 2

--- discongas/tests/test_undisturbedremoval.py
+++ discongas/tests/test_undisturbedremoval.py
@@ -1,4 +1,17 @@
 from discongas.undisturbedremoval import *
 
-def test_a():
-  assert(a() == True)
+def test_roofpitchcorrection():
+  # test of exact values according to table 2
+  assert(roofpitchcorrection(20) == (0, 0.85))
+  assert(roofpitchcorrection(30) == (0, 0.70))
+  assert(roofpitchcorrection(45) == (10, 0.50))
+  assert(roofpitchcorrection(60) == (20, 0.45))
+
+  # test of values less than 20 or greater than 60
+  assert(roofpitchcorrection(19) == (0, 0))
+  assert(roofpitchcorrection(61) == (20, 0.45))
+
+  # test of interpolation 
+  assert(roofpitchcorrection(21) == (0, 0.83))
+  assert(roofpitchcorrection(37.5) == (5, 0.6))
+  assert(roofpitchcorrection(50) == (13.33, 0.48))
--- discongas/undisturbedremoval.py
+++ discongas/undisturbedremoval.py
@@ -1,2 +1,38 @@
-def a():
-  return True
+def roofpitchcorrection(alpha):
+  """
+  Calculate the roof pitch correction gamma and factor f as a function of the roof pitch alpha.
+
+  :param alpha: roof pitch:
+
+  :return: a tuple (gamma, f)
+  """
+  if alpha < 20:
+    return (0, 0)
+  if alpha >= 20 and alpha <= 30:
+    gamma = 0
+
+    # multiplier to interpolate
+    m = (alpha-20)/10
+
+    f = round(0.85 - 0.15 * m, 2)
+
+  elif alpha > 30 and alpha <= 45:
+    # multiplier to interpolate
+    m = (alpha-30)/15
+
+    gamma = round(10 * m, 2)
+    f = round(0.7 - 0.2 * m, 2)
+
+  elif alpha > 45 and alpha < 60:
+    # multiplier to interpolate
+    m = (alpha-45)/15
+
+    gamma = round(10 + 10 * m, 2)
+    f = round(0.5 - 0.05 * m, 2)
+
+  else:
+    gamma = 20
+    f = 0.45
+    
+  return (gamma, f)
+  
