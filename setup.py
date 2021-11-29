9770796a497b8bf275db5c926775992bde050e55
umweltschutzsoftware <jannik.huels@richtershuels.de>
2021-11-29 15:27:58 +0100

    installation

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
   name='discongas',
-  version='0.0.1',
-  description='A module that implements VDI 3781 Part 4 to compute the discharge conditions for exhaust gases of small and medium combustion systems and other installations',
+  version='1.0.0',
+  description='Implementation of VDI 3781 Part 4 to compute the discharge conditions for exhaust gases of small and medium combustion systems and other installations.',
   author='Ingenieurbüro Richters & Hüls',
   author_email='info@richtershuels.de',
-  packages=find_packages(include=['discongas']),
+  packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
   install_requires=[],
   setup_requires=['pytest-runner'],
   tests_require=['pytest==4.4.1'],
